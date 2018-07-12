cd95bb81b82bb11bb04e95a1798b10d33d945c98
Erlend Hammer <tkdhammer@gmail.com>
2018-07-12 13:25:56 +0200

    added some stuff

---
+++ .custom/bg_script.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+while true; do
+	feh --randomize --bg-max ~/Pictures/Wallpapers/*
+	sleep 600
+done
---
+++ .custom/screenshot.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+saveloc=/home/erlend/Bilder/screenshots/`date "+%F_%H%M%S.png"`
+
+maim -s $saveloc -c 0,1,1,0.2 -l -m 9 &&
+xclip -selection clipboard -t image/png -i $saveloc
+#notify-send -a "screenshot" "SCREEN SHOT TAKEN"
