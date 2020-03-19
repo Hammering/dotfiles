13d7067a0f0cf543af9dc9575b764cdf5ecb323f
Erlend Hammer <tkdhammer@gmail.com>
2020-03-19 16:36:31 +0100

    Changed to my new 3 screen setup

--- .custom/bg_script.sh
+++ .custom/bg_script.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 while true; do
-	feh --randomize --bg-max ~/Bilder/Wallpapers/*
-	sleep 600
+        #(( RANDOM%2 ==0 )) && feh --randomize --bg-max --no-xinerama ~/Bilder/Wallpapers/2\ screens/* || feh --randomize --bg-max ~/Bilder/Wallpapers/Landscape/*
+	feh --bg-max ~/Bilder/Wallpapers/Landscape/$(ls ~/Bilder/Wallpapers/Landscape/ | shuf -n 1) ~/Bilder/Wallpapers/Landscape/$(ls ~/Bilder/Wallpapers/Landscape/ | shuf -n 1) ~/Bilder/Wallpapers/Portrait/$(ls ~/Bilder/Wallpapers/Portrait/ | shuf -n 1)
+        sleep 300
 done
+
