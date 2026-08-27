9612718bd6f8585d234af03934c3d11c7c6e296b
Erlend Hammer <tkdhammer@gmail.com>
2026-08-27 19:34:37 +0200

    Fixed bg_script

--- .custom/bg_script.sh
+++ .custom/bg_script.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 while true; do
         #(( RANDOM%2 ==0 )) && feh --randomize --bg-max --no-xinerama ~/Bilder/Wallpapers/2\ screens/* || feh --randomize --bg-max ~/Bilder/Wallpapers/Landscape/*
-	feh --bg-max ~/Bilder/Wallpapers/2560x1440/$(ls ~/Bilder/Wallpapers/2560x1440/ | shuf -n 1) ~/Bilder/Wallpapers/Portrait/$(ls ~/Bilder/Wallpapers/Portrait/ | shuf -n 1) ~/Bilder/Wallpapers/Portrait/$(ls ~/Bilder/Wallpapers/Portrait/ | shuf -n 1) 
+	#feh --bg-max ~/Bilder/Wallpapers/Landscape_new/$(ls ~/Bilder/Wallpapers/Landscape_new/ | shuf -n 1) ~/Bilder/Wallpapers/Portrait_new/$(ls ~/Bilder/Wallpapers/Portrait_new/ | shuf -n 1) ~/Bilder/Wallpapers/Portrait_new/$(ls ~/Bilder/Wallpapers/Portrait_new/ | shuf -n 1) 
+	feh --bg-max $(find ~/Bilder/Wallpapers/Landscape_new/ | shuf -n 1) $(find ~/Bilder/Wallpapers/Portrait_new/ | shuf -n 1) $(find ~/Bilder/Wallpapers/Portrait_new | shuf -n 1)
         sleep 300
 done
 
